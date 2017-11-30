e2d87736daa3e667a2c684fe7c3f13a1d52b00c0
Nischal <nischaljakhar@gmail.com>
2017-11-30 07:41:17 +0530

    added post request syntax

--- fetch.js
+++ fetch.js
@@ -12,3 +12,26 @@ fetch('https://httpbin.org/ip').then(function (response) {
     console.log(err);
 
 });
+
+//POST request
+
+fetch('https://httpbin.org/post',{
+    method : 'POST',
+    headers : {
+        'Content-Type' : 'application/JSON',
+        'Accept' : 'application/JSON'
+    },
+    mode : 'cors',
+    body : JSON.stringify({message : 'Does this work?'})
+})
+    .then(function (response) {
+    console.log(response);
+    return response.json();
+})
+    .then(function(data){
+        console.log(data);
+    })
+    .catch(function (err) {
+        console.log(err);
+
+    });
